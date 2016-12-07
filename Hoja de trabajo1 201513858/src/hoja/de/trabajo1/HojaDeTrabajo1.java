ad9dad0a5e5d3938b77d42f7b5a4c354859bc19d
201513858 <201513858@ingenieria.usac.edu.gt>
2016-12-06 21:22:53 -0600

    Metodo 1 (palabras palindromas)

---
+++ Hoja de trabajo1 201513858/src/hoja/de/trabajo1/HojaDeTrabajo1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja.de.trabajo1;
+
+/**
+ *
+ * @author Cesar
+ */
+public class HojaDeTrabajo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new interfaz().setVisible(true);
+    }
+    
+}
